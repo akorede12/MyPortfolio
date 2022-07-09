3c3af3b8bcbfd1a33172ccf868c7873b9d3dfb68
akorede12 <akoredeaboaba12@gmail.com>
2022-07-09 01:14:51 -0700

    upload

---
+++ constants/data.js
@@ -0,0 +1,50 @@
+const userData = {
+  githubUsername: "akorede12",
+  name: "Aboaba Akorede",
+  designation: "Blockchain Developer, Frontend Programmer",
+  avatarUrl: "/Me2.jpg",
+  email: "akoredeaboaba12@gmail.com",
+  phone: "+234",
+  address: "Lagos, Nigeria.",
+  projects: [
+    {
+      title: "NFT Marketplace",
+      link: "https://akorede-s-nft-marketplace-git-rico-akorede12.vercel.app/",
+      imgUrl: "/Marketplace.png",
+    },
+    {
+      title: "WhiteList Dapp",
+      link: "https://whitelist-dapp-wheat-eta.vercel.app/",
+      imgUrl: "/WhiteListDapp.png",
+    }
+  ],
+  about: {
+    title:
+      "I'm a Lagos born techie, with a love for software development",
+    description: [
+      "fascinated with all things tech and how to use technology to improve my daily life and the lives of people around me"
+    ],
+    currentProject: "NFT Marketplace",
+    currentProjectUrl: "https://akorede-s-nft-marketplace-git-rico-akorede12.vercel.app/",
+  },
+  experience: [
+    {
+      title: "Blockchain Developer",
+      company: "myself",
+      year: "2022",
+      companyLink: "https://akorede-s-nft-marketplace-git-rico-akorede12.vercel.app/",
+      desc: "Built an NFT markeplace that allows users to create nfts and sell those nfts on the marketplace",
+    },
+  ],
+  resumeUrl:
+    "https://docs.google.com/file/d/1sCvnC_ocB2fkZHMykygdNWEQ96K-4Xr1/edit?usp=docslist_api&filetype=msword",
+  socialLinks: {
+    instagram: "https://instagram.com/tame.nation",
+    twitter: "https://twitter.com/tame_nation",
+    linkedin: "https://linkedin.com/in/akorede-aboaba-306b3716a",
+    github: "https://github.com/akorede12",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
