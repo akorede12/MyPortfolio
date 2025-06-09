d4efb4e81ccfa88da47899d49bd78d06d317f186
Akorede12 <akoredeaboaba12@gmail.com>
2025-06-09 02:06:56 +0100

    JUN 9 2025 -fix image error

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -40,7 +40,7 @@ export default function FavouriteProjects() {
           >
             <div className="relative overflow-hidden">
               <img
-                src="/SmartinvestHero.png"
+                src="/SmartInvestHero.png"
                 alt="SmartInvest"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
